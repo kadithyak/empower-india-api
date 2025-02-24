b66ca062caf106371f17d0c16bf4a38902c7b38e
nagarjunveni <v.nagarjun@gmail.com>
2025-02-25 00:22:14 +0530

    Update SponsorsController.java

--- src/main/java/com/andhraempower/controller/SponsorsController.java
+++ src/main/java/com/andhraempower/controller/SponsorsController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/")
+@RequestMapping("/Sponsors")
 @RequiredArgsConstructor
 public class SponsorsController {
 
-    @GetMapping("/")
+    @GetMapping("/getSponsors")
     public String getSponsors(){
         return "This is Sponsors page";
     }
